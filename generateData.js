2d7f5fd56e5789b31f0c20826b932ec89c2f4e42
mark-nakachon <harishchennupati54@gmail.com>
2019-09-06 01:03:25 +0530

    Completed

--- generateData.js
+++ generateData.js
@@ -1,43 +1,43 @@
 const faker = require ('faker');
 
-var database = {data: [],devices:[]};
+var database = {devices: []};
 
-for (var i = 1; i <= 5; i++) {
-  database.data.push ({
-    id: i,
-    orderno: faker.random.alphaNumeric ((number = 10)),
-    itemno: faker.random.alphaNumeric ((number = 5)),
-    devices: [],
-  });
-}
 for (var i = 0; i < 5; ++i) {
+  const orderno = faker.random.alphaNumeric ((number = 10));
+  const itemno =  faker.random.alphaNumeric ((number = 5));
   for (var j = 1; j <= 5; j++) {
-    const model = faker.random.alphaNumeric ((number = 5));
-    const id = faker.random.alphaNumeric((number = 5));
-    database.data[i].devices.push ({
-      deviceId:id,
-      deviceStatus: 'configured',
-      model:model,
-      location: `Structure ${j} compartment name`,
-      deviceIssue: `An issue reported`,
-      configuredBy: faker.internet.userName (),
-      configFileBy: faker.internet.userName (),
-    });
+    const messages = ["success","error"];
+    const status = ["configured","not configured"]
     database.devices.push({
-      model:model,
-      deviceId:id,
+      orderno:orderno ,
+      itemno:itemno,
+      model:faker.random.alphaNumeric ((number = 5)),
+      deviceId:faker.random.alphaNumeric((number = 5)),
       Family:'Meter',
-      model:model,
+      location:`Structure ${j} compartment name`,
+      devicePhoto:faker.image.imageUrl(),
       styleNo:faker.phone.phoneNumber(),
       configFileName:faker.system.fileName(),
+      configuredBy: faker.internet.userName (),
+      configFileBy: faker.internet.userName (),
       compartment:'starter',
-      status:'configured',
+      status:status[faker.random.number({min:0,max:1})],
       labelName:faker.system.fileName(),
       Name:faker.commerce.productName(),
+      location: `Structure ${j} compartment name`,
+      deviceIssue: `An issue reported`,
       CTPrimary:'10000',
-      CTSecondary:'10'
+      CTSecondary:'10',
+      PTPrimary:'10',
+      PTSecondary:'1000',
+      Wiring:'3 phase 4 phase',
+      IpAddress:faker.internet.ip(),
+      Subnet:'255.0.0.0',
+      deviceconnected : messages[faker.random.number({min:0, max:1})],
+      validatingfirmware :messages[faker.random.number({min:0, max:1})],
+      uploadfile: messages[faker.random.number({min:0, max:1})],
+      rebootdevice: messages[faker.random.number({min:0, max:1})]
     })
-
   }
 }
 console.log (JSON.stringify (database));
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const server = jsonServer.create();
 const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults();
 const userdb = JSON.parse(fs.readFileSync('./users.json','UTF-8'))
+const db = JSON.parse(fs.readFileSync('./db.json','UTF-8'));
 const config = require('./config');
-const { check, validationResult } = require('express-validator');
+const { check, validationResult,query } = require('express-validator/check');
 const tokenList = {}
 
 server.use(jsonServer.defaults());
@@ -15,27 +16,33 @@ server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(middlewares);
 
-function isAuthenticated({email, password,applicationId}){
-    return userdb.users.findIndex(user => user.email === email && user.password === password && user.applicationId === applicationId) !== -1
+
+function isAuthenticated({email, password}){
+    return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
 }
 
-server.post('/login',(req,res)=>{
-    req.checkBody('email', 'email is required').notEmpty();
-    req.checkBody('password', 'password is required').notEmpty();
-    req.checkBody('applicationId', 'app id is required').notEmpty();
+server.get('/test',(req,res)=>{
+    res.status(200).json({error:'hi'})
+})
 
-    let errors = req.validationErrors();
-    if (errors) {
-        res.send('There have been validation errors: ' + util.inspect(errors), 400);
-        return;
+server.post('/login',[check('email','email is required').not().isEmpty(),
+                      check('password','password is required').not().isEmpty(),
+                      check('applicationId','app id is required').optional()
+                    ],(req,res)=>{
+
+    let errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).jsonp(errors.array());
     }
+
     const {email,password,applicationId} = req.body;
 
     const user = {
         "email":email,
     }
-    if (isAuthenticated({email, password,applicationId}) === false) {
-        const status = 401
+    if (isAuthenticated({email, password}) === false) {
+        const status = 404
         const message = 'Incorrect email or password'
         res.status(status).json({status, message})
         return
@@ -52,16 +59,15 @@ server.post('/login',(req,res)=>{
     }
 )
 
-server.post('/token', (req,res) => {
-    // refresh the damn token
-    req.checkBody('refreshToken', 'refreshToken is required').notEmpty();
+server.post('/token',[check('refreshToken','refresh Token is required').not().isEmpty(),
+                    ], (req,res) => {
 
-    let errors = req.validationErrors();
+    let errors = validationResult(req);
 
-    if (errors) {
-        res.send('There have been validation errors: ' + util.inspect(errors), 400);
-        return;
+    if(!errors.isEmpty()){
+        return res.status(422).jsonp(errors.array());
     }
+
     const postData = req.body
     // if refresh token exists
     if((postData.refreshToken) && (postData.refreshToken in tokenList)) {
@@ -81,7 +87,127 @@ server.post('/token', (req,res) => {
 })
 server.use(require('./tokenChecker'))
 
+server.get('/api/locate/order',[query('orderno','must be 10 chars').not().isEmpty().isLength({min:10,max:10}),
+                                query('itemno','must be 5 chars').not().isEmpty().isLength({min:5,max:5})],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).jsonp(errors.array());
+    }
+
+    let orderno = req.query.orderno;
+    let itemno = req.query.itemno;
+
+    let devices = [];
+    db.devices.map(device=>{
+    if(device.orderno === orderno && device.itemno === itemno){
+        devices.push(device);
+    }
+    })
+    const response = [];
+    const devicesneedConfigure = []
+    devices.map((device)=>{
+        const {status,model,configuredBy,location,deviceIssue,configFileBy} = device;
+        if(status !== "configured"){
+            devicesneedConfigure.push({model,status})
+        }
+        response.push({status,model,configuredBy,location,deviceIssue,configFileBy});
+    })
+
+    if(response.length!==0){
+        res.status(200).json({
+            totalDevices: response.length,
+            devices:response,
+            itemsToConfigure:devicesneedConfigure.length,
+            devicesneedConfigure:devicesneedConfigure
+        });
+    }
+    else{
+        res.status(404).jsonp({
+            error:'Not Found'
+        })
+    }
+})
+
+server.get('/api/configure/device',[query('model').not().isEmpty()],(req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).jsonp(errors.array());
+    }
+    let model = req.query.model;
+    let result = db.devices.find(devices=>{
+        if(devices.model === model){
+            return devices;
+        }
+    })
+    if(result){
+        let {
+            deviceconnected,
+            validatingfirmware,
+            uploadfile,
+            rebootdevice } = result;
+        let deviceResponse;
+        if(deviceconnected === 'error' || validatingfirmware === 'error' || uploadfile === 'error' || rebootdevice === 'error'){
+            deviceResponse = 'error';
+        }
+        else{
+            deviceResponse = 'success';
+        }
+        const response = {
+            "deviceconnected": deviceconnected,
+            "validatingfirmware": validatingfirmware,
+            "uploadfile": uploadfile,
+            "rebootdevice": rebootdevice,
+            "deviceResponse":deviceResponse
+        }
+        res.status(200).json(response);
+    }
+    else{
+        res.status(404).jsonp({
+            error:'Not Found'
+        })
+    }
+})
+
+server.use(jsonServer.rewriter({
+    '/api/search/order?*':'/devices'
+}))
+
+
 server.use(router);
 
-//server.use('/api',)
+router.render = function (req, res) {
+    if (req.url === '/devices' ) {
+      const {orderno,itemno} = req.query;
+      if(orderno === undefined || orderno === ''){
+          return res.status(422).jsonp({error:'orderno must not be empty'});
+      }
+      if(itemno === undefined || itemno === ''){
+          return res.status(422).jsonp({error:'itemno must not be empty'});
+      }
+      let devices = res.locals.data;
+      const response = [];
+      const devicesneedConfigure = []
+      devices.map((device)=>{
+          const {status,model,configuredBy,location,deviceIssue,configFileBy} = device;
+          if(status !== "configured"){
+            devicesneedConfigure.push({model,status})
+            }
+          response.push({status,model,configuredBy,location,deviceIssue,configFileBy});
+      })
+      res.jsonp({
+        totalDevices: response.length,
+        devices:response,
+        itemsToConfigure:devicesneedConfigure.length,
+        devicesneedConfigure:devicesneedConfigure
+      })
+    }
+    else{
+        res.jsonp({
+            devices:res.locals.data
+        })
+    }
+}
 server.listen(config.port || process.env.PORT || 3000);
+
+module.exports = server
